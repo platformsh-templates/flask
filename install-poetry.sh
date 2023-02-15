5ba46073251e8bf67ecfde6cbb1e1b2df58c5d3a
chadcarlson <chad.carlson@platform.sh>
2023-02-15 14:34:20 -0500

    Clean up poetry.

--- install-poetry.sh
+++ install-poetry.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-# Install poetry
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/install-poetry.py >> install-poetry.py
-python install-poetry.py --version $POETRY_VERSION
-# Source the Poetry command.
-. $PLATFORM_APP_DIR/.poetry/env
-# Add Poetry to .bash_profile, so available during SSH.
-echo ". $PLATFORM_APP_DIR/.poetry/env" >> ~/.bash_profile
+# Install poetry.
+export PIP_USER=false
+curl -sSL https://install.python-poetry.org | python3 - --version $POETRY_VERSION
+export PATH="/app/.local/bin:$PATH"
+export PIP_USER=true
+
+# Check lockfile and environment.
+poetry env use system
+poetry check
