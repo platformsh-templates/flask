684f59b96fe68038c2cb8a5f4f811fe725e7c445
Paul Gilzow <paul.gilzow@platform.sh>
2023-05-08 12:09:47 -0500

    install poetry first, then upgrade pip via poetry

--- install-poetry.sh
+++ install-poetry.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-# Upgrade pip
-python3.10 -m pip install --upgrade pip
-
 # Install poetry.
 export PIP_USER=false
 curl -sSL https://install.python-poetry.org | python3 - --version $POETRY_VERSION
 export PATH="/app/.local/bin:$PATH"
 export PIP_USER=true
+
+# Upgrade pip
+# python3.10 -m pip install --upgrade pip
+poetry run python -m pip install --upgrade pip
 
 # Check lockfile and environment.
 poetry env use system
